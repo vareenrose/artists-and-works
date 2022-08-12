b364df58ced8d230d78f5b3d2b7e19bb98aef1d1
vareenrose <vareen.agunda@gmail.com>
2022-08-12 10:13:14 +0300

    create post page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import Home from './components/Home'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Profile from './components/Profile';
+import NavBar from './components/NavBar';
+import PostImage from './components/PostImage';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <NavBar/>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='login' element={<SignIn />} />
           <Route path='signup' element={<SignUp />} />
+          <Route path='profile' element={<Profile />} />
+          <Route path='create' element={<PostImage />} />
         </Routes>
       </BrowserRouter>
     </div>
